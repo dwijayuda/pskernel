f1179573012bf8193b2dd3c8c9e21bddb606b449
Dwija Yuda <dwijayuda@gmail.com>
2026-09-22 21:35:11 +0700

    kernel: preserve native evaluator in WF sub-checkers

--- src/kernel/primitive/wf.ts
+++ src/kernel/primitive/wf.ts
@@ -7,7 +7,7 @@ import { N } from '../names.js';
 import { TypeChecker } from '../type-checker.js';
 
 function child(tc:TypeChecker,lctx:LocalContext):TypeChecker {
-  return new TypeChecker(tc.env,lctx,tc.state,tc.limits,tc.definitionSafety,tc.allowedLevelParams);
+  return new TypeChecker(tc.env,lctx,tc.state,tc.limits,tc.definitionSafety,tc.allowedLevelParams,false,tc.nativeEvaluator);
 }
 
 function containsFVarId(e:Expr,id:string):boolean {

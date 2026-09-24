fe33f64406ca144f813b3e94c53350ccae37b73c
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 00:22:37 +0700

    feat(selfhost): add immutable declaration environment

---
+++ selfhost/packages/environment/src/Ps/Environment/Basic.lean
@@ -0,0 +1,36 @@
+import Ps.Foundation.Name
+import Ps.Core.Declaration
+
+def psDeclarationName : PsDeclaration -> PsName
+  | .axiom name _ _ => name
+  | .definition name _ _ _ => name
+  | .theorem name _ _ _ => name
+  | .opaque name _ _ _ => name
+
+structure PsEnvironment where
+  declarations : List PsDeclaration
+
+def psEnvironmentEmpty : PsEnvironment :=
+  { declarations := [] }
+
+def psEnvironmentFindInList (name : PsName) : List PsDeclaration -> Option PsDeclaration
+  | [] => none
+  | declaration :: rest =>
+      if psNameEq name (psDeclarationName declaration) then
+        some declaration
+      else
+        psEnvironmentFindInList name rest
+
+def psEnvironmentFind (environment : PsEnvironment) (name : PsName) : Option PsDeclaration :=
+  psEnvironmentFindInList name environment.declarations
+
+def psEnvironmentContains (environment : PsEnvironment) (name : PsName) : Bool :=
+  match psEnvironmentFind environment name with
+  | none => false
+  | some _ => true
+
+def psEnvironmentAdd (environment : PsEnvironment) (declaration : PsDeclaration) : Option PsEnvironment :=
+  if psEnvironmentContains environment (psDeclarationName declaration) then
+    none
+  else
+    some { declarations := declaration :: environment.declarations }

50babbcccbd7e35df5e58167b3f14fc1a65fe8ca
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 02:09:55 +0700

    feat(selfhost): name Lean conditional builtins

--- selfhost/packages/core/src/Ps/Core/Builtin.lean
+++ selfhost/packages/core/src/Ps/Core/Builtin.lean
@@ -14,3 +14,21 @@ def psBoolName : PsName :=
 
 def psUnitName : PsName :=
   psRootName "Unit"
+
+def psBoolTrueName : PsName :=
+  psNameAppendStr psBoolName "true"
+
+def psBoolFalseName : PsName :=
+  psNameAppendStr psBoolName "false"
+
+def psEqName : PsName :=
+  psRootName "Eq"
+
+def psDecidableName : PsName :=
+  psRootName "Decidable"
+
+def psBoolDecEqName : PsName :=
+  psNameAppendStr psBoolName "decEq"
+
+def psIteName : PsName :=
+  psRootName "ite"

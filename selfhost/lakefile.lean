527cc66151c71c03b6702b480847c887aeaa0672
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 00:18:50 +0700

    build(selfhost): add responsibility-driven Lake libraries

--- selfhost/lakefile.lean
+++ selfhost/lakefile.lean
@@ -1,13 +1,21 @@
 import Lake
 open Lake DSL
 
-package proofscriptSelfhost where
-  srcDir := "src"
+package proofscriptSelfhost
 
-lean_lib ProofScriptCompiler where
-  srcDir := "../../packages/compiler/src"
-  roots := #[`ProofScript.Compiler.Data]
+lean_lib PsFoundation where
+  srcDir := "packages/foundation/src"
+  roots := #[
+    `Ps.Foundation.Name,
+    `Ps.Foundation.Source,
+    `Ps.Foundation.Diagnostic
+  ]
+
+lean_lib PsSyntax where
+  srcDir := "packages/syntax/src"
+  roots := #[`Ps.Syntax.Token]
 
 @[default_target]
 lean_exe psc1 where
+  srcDir := "src"
   root := `Main

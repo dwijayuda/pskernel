4ce117ef6fd2603b3283b9796ef15645262a1f43
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 00:48:01 +0700

    feat(selfhost): add free-variable abstraction

---
+++ selfhost/packages/core/src/Ps/Core/Abstract.lean
@@ -0,0 +1,36 @@
+import Ps.Core.Expr
+
+def psExprAbstractFVarAt (target : Nat) (depth : Nat) : PsExpr -> PsExpr
+  | .fvar id =>
+      if id == target then
+        PsExpr.bvar depth
+      else
+        PsExpr.fvar id
+  | .app fn arg =>
+      PsExpr.app
+        (psExprAbstractFVarAt target depth fn)
+        (psExprAbstractFVarAt target depth arg)
+  | .lam name type body binder =>
+      PsExpr.lam
+        name
+        (psExprAbstractFVarAt target depth type)
+        (psExprAbstractFVarAt target (depth + 1) body)
+        binder
+  | .forallE name type body binder =>
+      PsExpr.forallE
+        name
+        (psExprAbstractFVarAt target depth type)
+        (psExprAbstractFVarAt target (depth + 1) body)
+        binder
+  | .letE name type value body =>
+      PsExpr.letE
+        name
+        (psExprAbstractFVarAt target depth type)
+        (psExprAbstractFVarAt target depth value)
+        (psExprAbstractFVarAt target (depth + 1) body)
+  | .proj typeName index value =>
+      PsExpr.proj typeName index (psExprAbstractFVarAt target depth value)
+  | expr => expr
+
+def psExprAbstractFVar (target : Nat) (expr : PsExpr) : PsExpr :=
+  psExprAbstractFVarAt target 0 expr

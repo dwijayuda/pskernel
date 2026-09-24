34ca11afa6febcf5f7b39c3b7a27d0d8eb6c0b60
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 00:48:07 +0700

    feat(selfhost): add universe parameter instantiation

---
+++ selfhost/packages/core/src/Ps/Core/LevelSubst.lean
@@ -0,0 +1,77 @@
+import Ps.Core.Expr
+
+def psFindLevelArgument
+    (parameters : List PsName)
+    (arguments : List PsLevel)
+    (target : PsName) : Option PsLevel :=
+  match parameters, arguments with
+  | parameter :: parameterRest, argument :: argumentRest =>
+      if psNameEq parameter target then
+        some argument
+      else
+        psFindLevelArgument parameterRest argumentRest target
+  | _, _ => none
+
+def psLevelInstantiateParams
+    (parameters : List PsName)
+    (arguments : List PsLevel) : PsLevel -> PsLevel
+  | .zero => PsLevel.zero
+  | .succ value =>
+      PsLevel.succ (psLevelInstantiateParams parameters arguments value)
+  | .max left right =>
+      PsLevel.max
+        (psLevelInstantiateParams parameters arguments left)
+        (psLevelInstantiateParams parameters arguments right)
+  | .imax left right =>
+      PsLevel.imax
+        (psLevelInstantiateParams parameters arguments left)
+        (psLevelInstantiateParams parameters arguments right)
+  | .param name =>
+      match psFindLevelArgument parameters arguments name with
+      | none => PsLevel.param name
+      | some value => value
+  | .mvar id => PsLevel.mvar id
+
+def psLevelListInstantiateParams
+    (parameters : List PsName)
+    (arguments : List PsLevel) : List PsLevel -> List PsLevel
+  | [] => []
+  | level :: rest =>
+      psLevelInstantiateParams parameters arguments level
+        :: psLevelListInstantiateParams parameters arguments rest
+
+def psExprInstantiateLevelParams
+    (parameters : List PsName)
+    (arguments : List PsLevel) : PsExpr -> PsExpr
+  | .sortE level =>
+      PsExpr.sortE (psLevelInstantiateParams parameters arguments level)
+  | .constE name levels =>
+      PsExpr.constE name (psLevelListInstantiateParams parameters arguments levels)
+  | .app fn arg =>
+      PsExpr.app
+        (psExprInstantiateLevelParams parameters arguments fn)
+        (psExprInstantiateLevelParams parameters arguments arg)
+  | .lam name type body binder =>
+      PsExpr.lam
+        name
+        (psExprInstantiateLevelParams parameters arguments type)
+        (psExprInstantiateLevelParams parameters arguments body)
+        binder
+  | .forallE name type body binder =>
+      PsExpr.forallE
+        name
+        (psExprInstantiateLevelParams parameters arguments type)
+        (psExprInstantiateLevelParams parameters arguments body)
+        binder
+  | .letE name type value body =>
+      PsExpr.letE
+        name
+        (psExprInstantiateLevelParams parameters arguments type)
+        (psExprInstantiateLevelParams parameters arguments value)
+        (psExprInstantiateLevelParams parameters arguments body)
+  | .proj typeName index value =>
+      PsExpr.proj
+        typeName
+        index
+        (psExprInstantiateLevelParams parameters arguments value)
+  | expr => expr

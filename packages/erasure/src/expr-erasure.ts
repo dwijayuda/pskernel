be9cc10a1fa8028fa298d8242b03be9ae276f227
Dwija Yuda <dwijayuda@gmail.com>
2026-09-24 13:26:45 +0700

    fix(erasure): translate generic structure projection indices

--- packages/erasure/src/expr-erasure.ts
+++ packages/erasure/src/expr-erasure.ts
@@ -223,7 +223,7 @@ export function eraseRuntimeExpr(
         );
       }
       const field=structure.fields.find(
-        (item)=>item.sourceIndex===expr.index,
+        (item)=>item.sourceIndex===structure.numParams+expr.index,
       );
       if(field===undefined){
         throw new Error(

454b43588cce20a56e41873e09d0a05140bf348a
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 02:47:57 +0700

    feat(selfhost): expose inductive metadata lookup

--- selfhost/packages/environment/src/Ps/Environment/Basic.lean
+++ selfhost/packages/environment/src/Ps/Environment/Basic.lean
@@ -6,22 +6,42 @@ def psDeclarationName : PsDeclaration -> PsName
   | .definitionDecl name _ _ _ => name
   | .theoremDecl name _ _ _ => name
   | .opaqueDecl name _ _ _ => name
-
+  | .inductiveDecl info => info.name
+  | .constructorDecl info => info.name
+  | .recursorDecl info => info.name
 
 def psDeclarationLevelParams : PsDeclaration -> List PsName
   | .axiomDecl _ levelParams _ => levelParams
   | .definitionDecl _ levelParams _ _ => levelParams
   | .theoremDecl _ levelParams _ _ => levelParams
   | .opaqueDecl _ levelParams _ _ => levelParams
+  | .inductiveDecl info => info.levelParams
+  | .constructorDecl info => info.levelParams
+  | .recursorDecl info => info.levelParams
 
 def psDeclarationType : PsDeclaration -> PsExpr
   | .axiomDecl _ _ type => type
   | .definitionDecl _ _ type _ => type
   | .theoremDecl _ _ type _ => type
   | .opaqueDecl _ _ type _ => type
+  | .inductiveDecl info => info.type
+  | .constructorDecl info => info.type
+  | .recursorDecl info => info.type
 
 def psDeclarationValue : PsDeclaration -> Option PsExpr
   | .definitionDecl _ _ _ value => some value
+  | _ => none
+
+def psDeclarationInductiveInfo : PsDeclaration -> Option PsInductiveInfo
+  | .inductiveDecl info => some info
+  | _ => none
+
+def psDeclarationConstructorInfo : PsDeclaration -> Option PsConstructorInfo
+  | .constructorDecl info => some info
+  | _ => none
+
+def psDeclarationRecursorInfo : PsDeclaration -> Option PsRecursorInfo
+  | .recursorDecl info => some info
   | _ => none
 
 structure PsEnvironment where
@@ -40,6 +60,27 @@ def psEnvironmentFindInList (name : PsName) : List PsDeclaration -> Option PsDec
 
 def psEnvironmentFind (environment : PsEnvironment) (name : PsName) : Option PsDeclaration :=
   psEnvironmentFindInList name environment.declarations
+
+def psEnvironmentFindInductive
+    (environment : PsEnvironment)
+    (name : PsName) : Option PsInductiveInfo :=
+  match psEnvironmentFind environment name with
+  | some declaration => psDeclarationInductiveInfo declaration
+  | none => none
+
+def psEnvironmentFindConstructor
+    (environment : PsEnvironment)
+    (name : PsName) : Option PsConstructorInfo :=
+  match psEnvironmentFind environment name with
+  | some declaration => psDeclarationConstructorInfo declaration
+  | none => none
+
+def psEnvironmentFindRecursor
+    (environment : PsEnvironment)
+    (name : PsName) : Option PsRecursorInfo :=
+  match psEnvironmentFind environment name with
+  | some declaration => psDeclarationRecursorInfo declaration
+  | none => none
 
 def psEnvironmentContains (environment : PsEnvironment) (name : PsName) : Bool :=
   match psEnvironmentFind environment name with

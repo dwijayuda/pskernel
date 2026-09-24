c83bfc946474a1b3cf6c62ee1200dd2af8a8ab01
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 04:28:56 +0700

    feat(selfhost): print Lean structures

--- selfhost/packages/syntax/src/Ps/Syntax/PrintLean.lean
+++ selfhost/packages/syntax/src/Ps/Syntax/PrintLean.lean
@@ -173,6 +173,25 @@ def psPrintLeanBinder
                 (delimiters.1 ++ name ++ " : " ++
                   printedType ++ delimiters.2)
 
+def psPrintLeanStructureField
+    (field : PsSyntaxBinderHead × PsSyntaxTerm) :
+    Except PsSourcePrintError String :=
+  match field with
+  | (head, type) =>
+      match psPrintSyntaxName head.name with
+      | Except.error error => Except.error error
+      | Except.ok name =>
+          match psPrintLeanTerm type with
+          | Except.error error => Except.error error
+          | Except.ok printedType =>
+              let value :=
+                match head.kind with
+                | .explicit => name ++ " : " ++ printedType
+                | .implicit => "{" ++ name ++ " : " ++ printedType ++ "}"
+                | .strictImplicit => "{{" ++ name ++ " : " ++ printedType ++ "}}"
+                | .instanceImplicit => "[" ++ name ++ " : " ++ printedType ++ "]"
+              Except.ok ("  " ++ value)
+
 def psPrintLeanConstructor
     (constructor : PsSyntaxInductiveConstructor) :
     Except PsSourcePrintError String :=
@@ -260,6 +279,22 @@ def psPrintLeanDeclaration
                           paramSuffix ++ printedResult ++
                           " where\n" ++
                           psPrintJoin "\n" printedConstructors)
+  | .structureDecl name params fields _ =>
+      match psPrintSyntaxName name with
+      | Except.error error => Except.error error
+      | Except.ok printedName =>
+          match params.mapM psPrintLeanBinder with
+          | Except.error error => Except.error error
+          | Except.ok printedParams =>
+              match fields.mapM psPrintLeanStructureField with
+              | Except.error error => Except.error error
+              | Except.ok printedFields =>
+                  let paramSuffix :=
+                    if printedParams.isEmpty then ""
+                    else " " ++ psPrintJoin " " printedParams
+                  Except.ok
+                    ("structure " ++ printedName ++ paramSuffix ++
+                      " where\n" ++ psPrintJoin "\n" printedFields)
 
 def psPrintLeanModule
     (module : PsSyntaxModule) :

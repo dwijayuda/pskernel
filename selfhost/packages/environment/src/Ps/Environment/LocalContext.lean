4746ce017222319a9d17c5b77138d268bfeacc52
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 00:41:00 +0700

    feat(selfhost): add explicit local context

---
+++ selfhost/packages/environment/src/Ps/Environment/LocalContext.lean
@@ -0,0 +1,90 @@
+import Ps.Foundation.Name
+import Ps.Core.Expr
+
+inductive PsLocalDecl where
+  | binding
+      (id : Nat)
+      (userName : PsName)
+      (type : PsExpr)
+      (binder : PsBinderInfo)
+  | letDecl
+      (id : Nat)
+      (userName : PsName)
+      (type : PsExpr)
+      (value : PsExpr)
+
+structure PsLocalContext where
+  nextId : Nat
+  declarations : List PsLocalDecl
+
+structure PsLocalPushResult where
+  context : PsLocalContext
+  id : Nat
+
+def psLocalEmpty : PsLocalContext :=
+  { nextId := 0, declarations := [] }
+
+def psLocalDeclId : PsLocalDecl -> Nat
+  | .binding id _ _ _ => id
+  | .letDecl id _ _ _ => id
+
+def psLocalDeclUserName : PsLocalDecl -> PsName
+  | .binding _ userName _ _ => userName
+  | .letDecl _ userName _ _ => userName
+
+def psLocalFindByIdInList (id : Nat) : List PsLocalDecl -> Option PsLocalDecl
+  | [] => none
+  | declaration :: rest =>
+      if psLocalDeclId declaration == id then
+        some declaration
+      else
+        psLocalFindByIdInList id rest
+
+def psLocalFindById (context : PsLocalContext) (id : Nat) : Option PsLocalDecl :=
+  psLocalFindByIdInList id context.declarations
+
+def psLocalContainsId (context : PsLocalContext) (id : Nat) : Bool :=
+  match psLocalFindById context id with
+  | none => false
+  | some _ => true
+
+def psLocalFindUserInList (userName : PsName) : List PsLocalDecl -> Option PsLocalDecl
+  | [] => none
+  | declaration :: rest =>
+      if psNameEq userName (psLocalDeclUserName declaration) then
+        some declaration
+      else
+        psLocalFindUserInList userName rest
+
+def psLocalFindUser (context : PsLocalContext) (userName : PsName) : Option PsLocalDecl :=
+  psLocalFindUserInList userName context.declarations
+
+def psLocalPushBinding
+    (context : PsLocalContext)
+    (userName : PsName)
+    (type : PsExpr)
+    (binder : PsBinderInfo) : PsLocalPushResult :=
+  let id := context.nextId
+  let declaration := PsLocalDecl.binding id userName type binder
+  {
+    context := {
+      nextId := id + 1
+      declarations := declaration :: context.declarations
+    }
+    id := id
+  }
+
+def psLocalPushLet
+    (context : PsLocalContext)
+    (userName : PsName)
+    (type : PsExpr)
+    (value : PsExpr) : PsLocalPushResult :=
+  let id := context.nextId
+  let declaration := PsLocalDecl.letDecl id userName type value
+  {
+    context := {
+      nextId := id + 1
+      declarations := declaration :: context.declarations
+    }
+    id := id
+  }

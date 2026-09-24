b06f26b70f7887ad420671449171308979ef88b6
Dwija Yuda <dwijayuda@gmail.com>
2026-09-24 23:52:08 +0700

    build(selfhost): build compiler workspace package with Lake

--- selfhost/lakefile.lean
+++ selfhost/lakefile.lean
@@ -4,7 +4,9 @@ open Lake DSL
 package proofscriptSelfhost where
   srcDir := "src"
 
-lean_lib ProofScript
+lean_lib ProofScriptCompiler where
+  srcDir := "../packages/compiler/src"
+  roots := #[`ProofScript.Compiler.Data]
 
 @[default_target]
 lean_exe psc1 where

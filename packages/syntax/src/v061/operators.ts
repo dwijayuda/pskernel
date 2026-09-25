61f8dba85d82be4e01032b1d3a3c57050705837a
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 22:08:34 +0700

    fix(k9): scope list append precedence to Lean subset

--- packages/syntax/src/v061/operators.ts
+++ packages/syntax/src/v061/operators.ts
@@ -1,9 +1,14 @@
 export const V061_BINARY_PRECEDENCE:ReadonlyMap<string,number>=new Map([
   ['||',1],['&&',2],['==',3],['!=',3],
   ['<',4],['<=',4],['>',4],['>=',4],
-  ['++',5],['+',5],['-',5],['*',6],['/',6],['%',6],
+  ['+',5],['-',5],['*',6],['/',6],['%',6],
 ]);
 
 export function v061BinaryPrecedence(operator:string):number|undefined {
   return V061_BINARY_PRECEDENCE.get(operator);
+}
+
+export function v061LeanSubsetBinaryPrecedence(operator:string):number|undefined {
+  if(operator==='++')return 5;
+  return v061BinaryPrecedence(operator);
 }

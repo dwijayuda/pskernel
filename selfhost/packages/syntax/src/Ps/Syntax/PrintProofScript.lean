53956a8c3b68335abca4256564e0f376493fd20f
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 19:21:42 +0700

    fix(selfhost): type ProofScript binder printer helpers

--- selfhost/packages/syntax/src/Ps/Syntax/PrintProofScript.lean
+++ selfhost/packages/syntax/src/Ps/Syntax/PrintProofScript.lean
@@ -158,7 +158,9 @@ def psPrintProofScriptTermWithFuel
                           (psPrintJoin ", " printedArgs)
                           ")")
       | .lambda binders body _ =>
-          let printBinder :=
+          let printBinder :
+              Prod PsSyntaxBinderHead PsSyntaxTerm ->
+                Except PsSourcePrintError String :=
             fun (binder : Prod PsSyntaxBinderHead PsSyntaxTerm) =>
               match binder with
               | Prod.mk head type =>
@@ -194,7 +196,9 @@ def psPrintProofScriptTermWithFuel
                       " => "
                       printedBody)
       | .forallE binders body _ =>
-          let printBinder :=
+          let printBinder :
+              Prod PsSyntaxBinderHead PsSyntaxTerm ->
+                Except PsSourcePrintError String :=
             fun (binder : Prod PsSyntaxBinderHead PsSyntaxTerm) =>
               match binder with
               | Prod.mk head type =>

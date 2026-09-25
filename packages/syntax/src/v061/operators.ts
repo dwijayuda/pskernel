6a586e3cc689c83a516a1dc011abacd47c13e4d9
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 21:58:07 +0700

    feat(k9): parse Lean list append notation

--- packages/syntax/src/v061/operators.ts
+++ packages/syntax/src/v061/operators.ts
@@ -1,7 +1,7 @@
 export const V061_BINARY_PRECEDENCE:ReadonlyMap<string,number>=new Map([
   ['||',1],['&&',2],['==',3],['!=',3],
   ['<',4],['<=',4],['>',4],['>=',4],
-  ['+',5],['-',5],['*',6],['/',6],['%',6],
+  ['++',5],['+',5],['-',5],['*',6],['/',6],['%',6],
 ]);
 
 export function v061BinaryPrecedence(operator:string):number|undefined {

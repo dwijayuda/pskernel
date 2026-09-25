8818b487b37a143b5f34aae862df6d034b1717a2
Dwija Yuda <dwijayuda@gmail.com>
2026-09-26 01:09:32 +0700

    refactor(selfhost): split projection constructor match

--- selfhost/packages/elab/src/Ps/Elab/Term.lean
+++ selfhost/packages/elab/src/Ps/Elab/Term.lean
@@ -199,39 +199,44 @@ def psElabProjectionStep
             Except.error PsElabError.unsupportedTerm
           else
             match info.constructors with
-            | List.cons constructorName List.nil =>
-                match
-                    psEnvironmentFindConstructor
-                      current.context.environment
-                      constructorName with
-                | none => Except.error PsElabError.unsupportedTerm
-                | some constructorInfo =>
+            | List.nil =>
+                Except.error PsElabError.unsupportedTerm
+            | List.cons constructorName remainingConstructors =>
+                match remainingConstructors with
+                | List.nil =>
                     match
-                        psElabProjectionApplyParameters
-                          current.context
-                          view.args
-                          constructorInfo.type with
-                    | Except.error error => Except.error error
-                    | Except.ok fieldCursor =>
+                        psEnvironmentFindConstructor
+                          current.context.environment
+                          constructorName with
+                    | none => Except.error PsElabError.unsupportedTerm
+                    | some constructorInfo =>
                         match
-                            psElabFindStructureField
+                            psElabProjectionApplyParameters
                               current.context
-                              typeName
-                              current.term
-                              fieldName
-                              0
-                              constructorInfo.numFields
-                              fieldCursor with
+                              view.args
+                              constructorInfo.type with
                         | Except.error error => Except.error error
-                        | Except.ok index =>
-                            psElabResolvedTerm
-                              current.context
-                              (PsExpr.proj
-                                typeName
-                                index
-                                current.term)
-                              none
-            | _ => Except.error PsElabError.unsupportedTerm
+                        | Except.ok fieldCursor =>
+                            match
+                                psElabFindStructureField
+                                  current.context
+                                  typeName
+                                  current.term
+                                  fieldName
+                                  0
+                                  constructorInfo.numFields
+                                  fieldCursor with
+                            | Except.error error => Except.error error
+                            | Except.ok index =>
+                                psElabResolvedTerm
+                                  current.context
+                                  (PsExpr.proj
+                                    typeName
+                                    index
+                                    current.term)
+                                  none
+                | List.cons _ _ =>
+                    Except.error PsElabError.unsupportedTerm
   | _ => Except.error PsElabError.unsupportedTerm
 
 def psElabProjectionChain

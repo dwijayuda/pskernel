21776f0c09c92bd9665a4b67053de1068dc966f9
Dwija Yuda <dwijayuda@gmail.com>
2026-09-26 05:08:28 +0700

    fix(selfhost): lower singleton constructor pattern

--- selfhost/packages/meta/src/Ps/Meta/Infer.lean
+++ selfhost/packages/meta/src/Ps/Meta/Infer.lean
@@ -177,7 +177,7 @@ def psInferProjectionType
               Except.error PsInferError.projectionUnsupported
             else
               match info.constructors with
-              | [constructorName] =>
+              | constructorName :: [] =>
                   match
                       psEnvironmentFindConstructor
                         environment

214679c545f14ac95aa5b0f8ab8411a0bbe9d6a6
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 07:50:19 +0700

    feat(selfhost): complete safe Array bootstrap API

--- selfhost/stdlib/ProofScript/Data/Array.lean
+++ selfhost/stdlib/ProofScript/Data/Array.lean
@@ -1,3 +1,5 @@
+import ProofScript.Data.Option
+
 def arrayEmptyWithCapacity {α : Type}
     (capacity : Nat) : Array α :=
   Array.emptyWithCapacity capacity
@@ -29,3 +31,74 @@ def arrayFoldl {α : Type} {β : Type}
     (start : Nat)
     (stop : Nat) : β :=
   Array.foldl f init xs start stop
+
+
+def arrayEmpty {α : Type}
+    (unit : Unit) : Array α :=
+  Array.emptyWithCapacity 0
+
+def arraySome {α : Type}
+    (value : α) : Option α :=
+  Option.some value
+
+def arrayGetOption {α : Type}
+    (xs : Array α) (index : Nat) : Option α :=
+  Array.getD
+    (Array.map arraySome xs)
+    index
+    Option.none
+
+def arraySet {α : Type}
+    (xs : Array α) (index : Nat) (value : α) : Array α :=
+  Array.setIfInBounds xs index value
+
+def arrayAnyStep {α : Type}
+    (predicate : α -> Bool)
+    (found : Bool)
+    (value : α) : Bool :=
+  if found then true else predicate value
+
+def arrayAny {α : Type}
+    (predicate : α -> Bool)
+    (xs : Array α) : Bool :=
+  Array.foldl
+    (arrayAnyStep predicate)
+    false
+    xs
+    0
+    (Array.size xs)
+
+def arrayAllStep {α : Type}
+    (predicate : α -> Bool)
+    (accepted : Bool)
+    (value : α) : Bool :=
+  if accepted then predicate value else false
+
+def arrayAll {α : Type}
+    (predicate : α -> Bool)
+    (xs : Array α) : Bool :=
+  Array.foldl
+    (arrayAllStep predicate)
+    true
+    xs
+    0
+    (Array.size xs)
+
+def arrayFindStep {α : Type}
+    (predicate : α -> Bool)
+    (found : Option α)
+    (value : α) : Option α :=
+  match found with
+  | Option.some current => Option.some current
+  | Option.none =>
+      if predicate value then Option.some value else Option.none
+
+def arrayFindOption {α : Type}
+    (predicate : α -> Bool)
+    (xs : Array α) : Option α :=
+  Array.foldl
+    (arrayFindStep predicate)
+    Option.none
+    xs
+    0
+    (Array.size xs)

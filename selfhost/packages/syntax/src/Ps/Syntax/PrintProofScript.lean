d7ea22045cf5a237f042d7bf12bb34bf9800f01e
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 19:26:51 +0700

    fix(selfhost): type ProofScript let printer result

--- selfhost/packages/syntax/src/Ps/Syntax/PrintProofScript.lean
+++ selfhost/packages/syntax/src/Ps/Syntax/PrintProofScript.lean
@@ -259,7 +259,8 @@ def psPrintProofScriptTermWithFuel
           match psPrintSyntaxName name with
           | Except.error error => Except.error error
           | Except.ok printedName =>
-              let printType :=
+              let printType :
+                  Except PsSourcePrintError String :=
                 match type with
                 | none => Except.ok ""
                 | some declaredType =>

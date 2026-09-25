a1a9cdb1cd9f0f92c4297b91d1f7ff9208d411fa
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 21:39:15 +0700

    refactor(selfhost): normalize environment construction

--- selfhost/packages/environment/src/Ps/Environment/Basic.lean
+++ selfhost/packages/environment/src/Ps/Environment/Basic.lean
@@ -32,32 +32,32 @@ def psDeclarationType : PsDeclaration -> PsExpr
   | .recursorDecl info => info.type
 
 def psDeclarationValue : PsDeclaration -> Option PsExpr
-  | .definitionDecl _ _ _ value => some value
-  | _ => none
+  | .definitionDecl _ _ _ value => Option.some value
+  | _ => Option.none
 
 def psDeclarationInductiveInfo : PsDeclaration -> Option PsInductiveInfo
   | .inductiveDecl info => some info
-  | _ => none
+  | _ => Option.none
 
 def psDeclarationConstructorInfo : PsDeclaration -> Option PsConstructorInfo
   | .constructorDecl info => some info
-  | _ => none
+  | _ => Option.none
 
 def psDeclarationRecursorInfo : PsDeclaration -> Option PsRecursorInfo
   | .recursorDecl info => some info
-  | _ => none
+  | _ => Option.none
 
 structure PsEnvironment where
   declarations : List PsDeclaration
 
 def psEnvironmentEmpty : PsEnvironment :=
-  { declarations := [] }
+  PsEnvironment.mk List.nil
 
 def psEnvironmentFindInList (name : PsName) : List PsDeclaration -> Option PsDeclaration
-  | [] => none
+  | [] => Option.none
   | declaration :: rest =>
       if psNameEq name (psDeclarationName declaration) then
-        some declaration
+        Option.some declaration
       else
         psEnvironmentFindInList name rest
 
@@ -69,21 +69,21 @@ def psEnvironmentFindInductive
     (name : PsName) : Option PsInductiveInfo :=
   match psEnvironmentFind environment name with
   | some declaration => psDeclarationInductiveInfo declaration
-  | none => none
+  | none => Option.none
 
 def psEnvironmentFindConstructor
     (environment : PsEnvironment)
     (name : PsName) : Option PsConstructorInfo :=
   match psEnvironmentFind environment name with
   | some declaration => psDeclarationConstructorInfo declaration
-  | none => none
+  | none => Option.none
 
 def psEnvironmentFindRecursor
     (environment : PsEnvironment)
     (name : PsName) : Option PsRecursorInfo :=
   match psEnvironmentFind environment name with
   | some declaration => psDeclarationRecursorInfo declaration
-  | none => none
+  | none => Option.none
 
 def psEnvironmentContains (environment : PsEnvironment) (name : PsName) : Bool :=
   match psEnvironmentFind environment name with
@@ -97,25 +97,25 @@ def psEnvironmentRemoveName
       if psNameEq name (psDeclarationName declaration) then
         psEnvironmentRemoveName name rest
       else
-        declaration :: psEnvironmentRemoveName name rest
+        List.cons declaration (psEnvironmentRemoveName name rest)
 
 def psEnvironmentAddReplacingAxiom
     (environment : PsEnvironment)
     (declaration : PsDeclaration) : Option PsEnvironment :=
-  let name := psDeclarationName declaration
+  let name := psDeclarationName declaration;
   match psEnvironmentFind environment name with
   | none =>
-      some { declarations := declaration :: environment.declarations }
+      Option.some (PsEnvironment.mk (List.cons declaration environment.declarations))
   | some (.axiomDecl _ _ _) =>
-      some {
-        declarations :=
-          declaration ::
-            psEnvironmentRemoveName name environment.declarations
-      }
-  | some _ => none
+      Option.some
+        (PsEnvironment.mk
+          (List.cons
+            declaration
+            (psEnvironmentRemoveName name environment.declarations)))
+  | some _ => Option.none
 
 def psEnvironmentAdd (environment : PsEnvironment) (declaration : PsDeclaration) : Option PsEnvironment :=
   if psEnvironmentContains environment (psDeclarationName declaration) then
-    none
+    Option.none
   else
-    some { declarations := declaration :: environment.declarations }
+    Option.some (PsEnvironment.mk (List.cons declaration environment.declarations))

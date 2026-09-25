6746bdd844a46d65c71431f9bb0abc869856bee5
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 23:27:06 +0700

    test(rust): add shared IR TS Rust differential fixture

---
+++ selfhost/test/BackendDifferentialFixture.lean
@@ -0,0 +1,261 @@
+import Ps.BackendTs.Module
+import Ps.BackendRust.Module
+
+def psBackendDiffModule : PsVerifiedIrModule :=
+  {
+    imports := []
+    structures := []
+    inductives := [
+      {
+        name := "Maybe"
+        typeParameters := [{ name := "A" }]
+        constructors := [
+          {
+            name := "none"
+            fields := []
+          },
+          {
+            name := "some"
+            fields := [
+              {
+                name := "value"
+                type := PsVerifiedIrType.typeParameter "A"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+    declarations := [
+      {
+        name := "diffNat"
+        typeParameters := []
+        parameters := [
+          {
+            name := "x"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+          },
+          {
+            name := "y"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+          }
+        ]
+        resultType := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+        body :=
+          PsVerifiedIrExpr.ifE
+            (PsVerifiedIrExpr.intrinsic
+              PsVerifiedIrIntrinsic.natLt
+              [
+                PsVerifiedIrExpr.var "x",
+                PsVerifiedIrExpr.var "y"
+              ])
+            (PsVerifiedIrExpr.intrinsic
+              PsVerifiedIrIntrinsic.natAdd
+              [
+                PsVerifiedIrExpr.var "x",
+                PsVerifiedIrExpr.var "y"
+              ])
+            (PsVerifiedIrExpr.intrinsic
+              PsVerifiedIrIntrinsic.natSub
+              [
+                PsVerifiedIrExpr.var "x",
+                PsVerifiedIrExpr.var "y"
+              ])
+      },
+      {
+        name := "diffInt"
+        typeParameters := []
+        parameters := [
+          {
+            name := "x"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.int
+          },
+          {
+            name := "y"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.int
+          }
+        ]
+        resultType := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.int
+        body :=
+          PsVerifiedIrExpr.intrinsic
+            PsVerifiedIrIntrinsic.intAdd
+            [
+              PsVerifiedIrExpr.intrinsic
+                PsVerifiedIrIntrinsic.intNeg
+                [PsVerifiedIrExpr.var "x"],
+              PsVerifiedIrExpr.var "y"
+            ]
+      },
+      {
+        name := "diffString"
+        typeParameters := []
+        parameters := [
+          {
+            name := "text"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.string
+          }
+        ]
+        resultType := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+        body :=
+          PsVerifiedIrExpr.intrinsic
+            PsVerifiedIrIntrinsic.stringUtf8ByteSize
+            [
+              PsVerifiedIrExpr.intrinsic
+                PsVerifiedIrIntrinsic.stringPush
+                [
+                  PsVerifiedIrExpr.var "text",
+                  PsVerifiedIrExpr.intrinsic
+                    PsVerifiedIrIntrinsic.charOfNat
+                    [
+                      PsVerifiedIrExpr.literal
+                        (PsVerifiedIrLiteral.natural 33)
+                    ]
+                ]
+            ]
+      },
+      {
+        name := "diffArray"
+        typeParameters := []
+        parameters := [
+          {
+            name := "a"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+          },
+          {
+            name := "b"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+          }
+        ]
+        resultType := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+        body :=
+          PsVerifiedIrExpr.letE
+            "xs"
+            (PsVerifiedIrExpr.intrinsic
+              PsVerifiedIrIntrinsic.arrayPush
+              [
+                PsVerifiedIrExpr.intrinsic
+                  PsVerifiedIrIntrinsic.arrayPush
+                  [
+                    PsVerifiedIrExpr.intrinsic
+                      PsVerifiedIrIntrinsic.arrayEmptyWithCapacity
+                      [
+                        PsVerifiedIrExpr.literal
+                          (PsVerifiedIrLiteral.natural 2)
+                      ],
+                    PsVerifiedIrExpr.var "a"
+                  ],
+                PsVerifiedIrExpr.var "b"
+              ])
+            (PsVerifiedIrExpr.letE
+              "ys"
+              (PsVerifiedIrExpr.intrinsic
+                PsVerifiedIrIntrinsic.arraySetIfInBounds
+                [
+                  PsVerifiedIrExpr.var "xs",
+                  PsVerifiedIrExpr.literal
+                    (PsVerifiedIrLiteral.natural 0),
+                  PsVerifiedIrExpr.literal
+                    (PsVerifiedIrLiteral.natural 10)
+                ])
+              (PsVerifiedIrExpr.intrinsic
+                PsVerifiedIrIntrinsic.arrayGetD
+                [
+                  PsVerifiedIrExpr.var "ys",
+                  PsVerifiedIrExpr.literal
+                    (PsVerifiedIrLiteral.natural 1),
+                  PsVerifiedIrExpr.literal
+                    (PsVerifiedIrLiteral.natural 99)
+                ]))
+      },
+      {
+        name := "diffMaybe"
+        typeParameters := []
+        parameters := [
+          {
+            name := "x"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+          },
+          {
+            name := "fallback"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+          },
+          {
+            name := "useSome"
+            type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.bool
+          }
+        ]
+        resultType := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+        body :=
+          PsVerifiedIrExpr.letE
+            "value"
+            (PsVerifiedIrExpr.ifE
+              (PsVerifiedIrExpr.var "useSome")
+              (PsVerifiedIrExpr.constructor
+                "Maybe"
+                "some"
+                [PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat]
+                [
+                  ("value", PsVerifiedIrExpr.var "x")
+                ])
+              (PsVerifiedIrExpr.constructor
+                "Maybe"
+                "none"
+                [PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat]
+                []))
+            (PsVerifiedIrExpr.matchE
+              "Maybe"
+              (PsVerifiedIrExpr.var "value")
+              [
+                (
+                  "none",
+                  [],
+                  PsVerifiedIrExpr.var "fallback"
+                ),
+                (
+                  "some",
+                  [
+                    {
+                      field := "value"
+                      name := "inner"
+                      type := PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.nat
+                    }
+                  ],
+                  PsVerifiedIrExpr.var "inner"
+                )
+              ])
+      }
+    ]
+  }
+
+def psBackendDiffEmitTs : Except String String :=
+  match psTsEmitModule psBackendDiffModule with
+  | Except.error _ =>
+      Except.error "TS"
+  | Except.ok output =>
+      Except.ok output
+
+def psBackendDiffEmitRust : Except String String :=
+  match psRustEmitModule psBackendDiffModule with
+  | Except.error _ =>
+      Except.error "RUST"
+  | Except.ok output =>
+      Except.ok output
+
+def main (args : List String) : IO Unit := do
+  match args with
+  | ["ts"] =>
+      match psBackendDiffEmitTs with
+      | Except.error target =>
+          throw (IO.userError ("PSC1_BACKEND_DIFF_EMIT_FAILED: " ++ target))
+      | Except.ok output =>
+          IO.print output
+  | ["rust"] =>
+      match psBackendDiffEmitRust with
+      | Except.error target =>
+          throw (IO.userError ("PSC1_BACKEND_DIFF_EMIT_FAILED: " ++ target))
+      | Except.ok output =>
+          IO.print output
+  | _ =>
+      throw
+        (IO.userError
+          "usage: psc1_backend_diff_fixture <ts|rust>")

57063e8b3412353576fc5b214d675e3bbe8c8540
Dwija Yuda <dwijayuda@gmail.com>
2026-09-26 01:15:06 +0700

    feat(wasm): model GC structures and aggregate instructions

--- selfhost/packages/backend-wasm/src/Ps/BackendWasm/Model.lean
+++ selfhost/packages/backend-wasm/src/Ps/BackendWasm/Model.lean
@@ -90,6 +90,18 @@ inductive PsWasmInstruction where
   | f64Le
   | f64Gt
   | f64Ge
+  | structNew (typeName : String)
+  | structGet (typeName : String) (fieldIndex : Nat)
+  | structGetS (typeName : String) (fieldIndex : Nat)
+  | structGetU (typeName : String) (fieldIndex : Nat)
+
+structure PsWasmStructField where
+  name : String
+  storageType : PsWasmStorageType
+
+structure PsWasmStructType where
+  name : String
+  fields : List PsWasmStructField
 
 structure PsWasmFunction where
   name : String
@@ -99,11 +111,13 @@ structure PsWasmFunction where
   body : List PsWasmInstruction
 
 structure PsWasmModule where
+  structures : List PsWasmStructType
   functions : List PsWasmFunction
   exports : List (String × String)
 
 def psWasmModuleEmpty : PsWasmModule :=
   {
+    structures := []
     functions := []
     exports := []
   }

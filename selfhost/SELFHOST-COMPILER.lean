df1c8df444862e79e77ddf72bd6ff52e70df019b
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 10:51:46 +0700

    test(selfhost): load owned Except in compiler aggregate

--- selfhost/SELFHOST-COMPILER.lean
+++ selfhost/SELFHOST-COMPILER.lean
@@ -2,6 +2,7 @@ import ProofScript.Data.Prod
 import ProofScript.Data.Option
 import ProofScript.Data.Ordering
 import ProofScript.Data.Result
+import ProofScript.Data.Except
 import ProofScript.Data.List
 import ProofScript.Data.Array
 import ProofScript.Data.Map

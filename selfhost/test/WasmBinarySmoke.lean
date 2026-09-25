6748ddf070dc74afae0d38b35a42e92b443a3a66
Dwija Yuda <dwijayuda@gmail.com>
2026-09-26 06:08:52 +0700

    test(wasm): isolate Nat multiplication recursion

--- selfhost/test/WasmBinarySmoke.lean
+++ selfhost/test/WasmBinarySmoke.lean
@@ -920,6 +920,54 @@ def psWasmSmokeIrModule : PsVerifiedIrModule :=
             ]
       },
       {
+        name := "natMulOneExact"
+        typeParameters := []
+        parameters := []
+        resultType :=
+          PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.bool
+        body :=
+          PsVerifiedIrExpr.intrinsic
+            PsVerifiedIrIntrinsic.natEq
+            []
+            [
+              PsVerifiedIrExpr.intrinsic
+                PsVerifiedIrIntrinsic.natMul
+                []
+                [
+                  PsVerifiedIrExpr.literal
+                    (PsVerifiedIrLiteral.natural 1),
+                  PsVerifiedIrExpr.literal
+                    (PsVerifiedIrLiteral.natural 7)
+                ],
+              PsVerifiedIrExpr.literal
+                (PsVerifiedIrLiteral.natural 7)
+            ]
+      },
+      {
+        name := "natMulOddExact"
+        typeParameters := []
+        parameters := []
+        resultType :=
+          PsVerifiedIrType.primitive PsVerifiedIrPrimitiveType.bool
+        body :=
+          PsVerifiedIrExpr.intrinsic
+            PsVerifiedIrIntrinsic.natEq
+            []
+            [
+              PsVerifiedIrExpr.intrinsic
+                PsVerifiedIrIntrinsic.natMul
+                []
+                [
+                  PsVerifiedIrExpr.literal
+                    (PsVerifiedIrLiteral.natural 3),
+                  PsVerifiedIrExpr.literal
+                    (PsVerifiedIrLiteral.natural 7)
+                ],
+              PsVerifiedIrExpr.literal
+                (PsVerifiedIrLiteral.natural 21)
+            ]
+      },
+      {
         name := "natMulExact"
         typeParameters := []
         parameters := []

4a54dcad8ae5cd2fcb79efc69d2f635e889ef0d1
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 23:34:56 +0700

    fix(selfhost): terminate inductive environment let

--- selfhost/packages/elab/src/Ps/Elab/Declaration.lean
+++ selfhost/packages/elab/src/Ps/Elab/Declaration.lean
@@ -1007,7 +1007,7 @@ def psElabInductiveDeclaration
                     let withInductiveResult :=
                       psEnvironmentAddOwnedBootstrapDeclaration
                         environment
-                        inductiveDeclaration
+                        inductiveDeclaration;
                     match withInductiveResult with
                     | none =>
                         Except.error

c70885aa041590222606edbd482a958f460a0cae
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 07:03:23 +0700

    feat(kernel): expose K1 environment modules

--- psc1-kernel/PSC1Kernel.lean
+++ psc1-kernel/PSC1Kernel.lean
@@ -2,3 +2,6 @@ import PSC1Kernel.Name
 import PSC1Kernel.Level
 import PSC1Kernel.Expr
 import PSC1Kernel.Instantiate
+import PSC1Kernel.Declaration
+import PSC1Kernel.Environment
+import PSC1Kernel.LocalContext

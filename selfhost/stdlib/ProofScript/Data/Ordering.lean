e2ab8cdd1db3035f38147a081872a578afb7adba
Dwija Yuda <dwijayuda@gmail.com>
2026-09-25 07:33:27 +0700

    feat(selfhost): add deterministic Nat ordering

--- selfhost/stdlib/ProofScript/Data/Ordering.lean
+++ selfhost/stdlib/ProofScript/Data/Ordering.lean
@@ -8,3 +8,12 @@ def orderingReverse (value : Ordering) : Ordering :=
   | Ordering.lt => Ordering.gt
   | Ordering.eq => Ordering.eq
   | Ordering.gt => Ordering.lt
+
+
+def orderingNat (left : Nat) (right : Nat) : Ordering :=
+  if Nat.blt left right then
+    Ordering.lt
+  else if Nat.blt right left then
+    Ordering.gt
+  else
+    Ordering.eq

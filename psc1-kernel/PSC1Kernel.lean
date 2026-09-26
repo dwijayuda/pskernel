cecccb51527aeb275015604f2d755ba4033f8b72
Dwija Yuda <dwijayuda@gmail.com>
2026-09-26 18:40:02 +0700

    build(kernel): export stateful checker module

--- psc1-kernel/PSC1Kernel.lean
+++ psc1-kernel/PSC1Kernel.lean
@@ -6,6 +6,8 @@ import PSC1Kernel.Declaration
 import PSC1Kernel.Environment
 import PSC1Kernel.LocalContext
 import PSC1Kernel.TypeChecker
+import PSC1Kernel.CheckerState
+import PSC1Kernel.CheckerStateful
 import PSC1Kernel.CheckerSession
 
 import PSC1Kernel.Kernel
